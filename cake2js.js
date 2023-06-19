3fd96c034fbb9627040f6b0167d5eea15013dfb9
abdulazeez <abdulazeez.aa258@gmail.com>
2023-06-19 16:03:39 +0530

    Completed

--- cake2js.js
+++ cake2js.js
@@ -10,3 +10,29 @@ xmark2.addEventListener('click',()=>{
     Filter.classList.remove('hide')
 })
 
+var down =document.getElementById('cw');
+var cake =document.getElementById('do');
+var newCake =document.getElementById('hello');
+var downNew =document.getElementById('how');
+
+
+cake.addEventListener('click',()=>{
+    
+    down.classList.toggle('hides')
+
+    
+
+})
+
+downNew.addEventListener('click',(event)=>{
+    
+    newCake.classList.toggle('hides')
+    event.stopPropagation()
+    
+
+})
+document.addEventListener('click',()=>{
+    if(newCake.classList.contains('hides')){
+        newCake.classList.remove('hides')
+    }
+})
